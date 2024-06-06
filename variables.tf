ec943ad930927cb61f58a60393b5270fb97186c8
beluvigishreegagana <beluvigishreegagana@gmail.com>
2024-06-06 12:04:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "os_type" {
+  type = string
+}
